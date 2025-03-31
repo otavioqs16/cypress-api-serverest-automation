f59c8c1c9b0b15f4d508986f675324310c1d462c
Otávio Queiroz <otavioqs16@gmail.com>
2025-03-31 15:28:28 -0300

    debug

--- cypress/e2e/usuarios.cy.js
+++ cypress/e2e/usuarios.cy.js
@@ -357,6 +357,7 @@ describe("ServeRest API Tests - Usuários", () => {
           data: { nome: "QA AUTOMATION 2" },
           token,
         }).then(({ response, data, _id }) => {
+          console.log(data);
           console.log(response.body);
           expect(response.status).to.eq(200);
           cy.getItem({ route: "usuarios", _id }).then((response) => {
